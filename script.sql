2b25903f5736d647c8e112165a6bd016553349f4
devchandani567 <chandu.d1314@gmail.com>
2026-03-12 02:06:15 +0530

    Modify cakes table and insert new cake records
    Updated the cakes table structure and added new entries.

--- script.sql
+++ script.sql
@@ -1,9 +1,12 @@
-create table cakes(s_no int(4) PRIMARY KEY,cakename varchar(16),Price int(4));
+
+create table cakes (s_no int(4) auto_increment primary key,CakeName Varchar(16),Price int(4));
 describe cakes;
-insert into cakes( cakename,Price) values('Mangocake',500);
-insert into cakes(cakename,Price) values('icecake',800);
-insert into cakes(cakename,Price) values('redvalvetcake',1200);
-insert into cakes(cakename,Price) values('vanillacake',600);
+insert into cakes(CakeName,Price) values('Mango cake',400);
+insert into cakes(CakeName,Price) values('chocolate cake',500);
+insert into cakes(CakeName,Price) values('RedValvet cake',400);
+insert into cakes(CakeName,Price) values('Strawberry cake',800);
+insert into cakes(CakeName,Price) values('Vanilla cake',600);
+insert into cakes(CakeName,Price) values('Ice cake',400);
 select count(*) from cakes;
 select max(Price) from cakes;
 select avg(Price) from cakes;
